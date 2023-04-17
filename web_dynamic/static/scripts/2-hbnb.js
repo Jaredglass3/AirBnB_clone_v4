a573d656a52e98eafd08c3701359414bcc036f27
Jaredglass3 <Jaredglass352@gmail.com>
2023-04-17 14:30:15 -0500

    Updated 2-hbnb.js for task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,11 +1,28 @@
-$('document').ready(function () {
-    let amenities = {};
-    $('INPUT[type="checkbox"]').change(function () {
+$(document).ready(function () {
+    // Send GET request to API
+    fetch('http://0.0.0.0:5001/api/v1/status/')
+      .then(response => {
+        // Check if response is OK
+        if (response.ok) {
+          // Add "available" class to div#api_status
+          document.querySelector('#api_status').classList.add('available');
+        } else {
+          // Remove "available" class from div#api_status
+          document.querySelector('#api_status').classList.remove('available');
+        }
+      })
+      .catch(error => console.error(error));
+  
+    // Show/Hide amenities
+    const amenitiesChecked = {};
+    $('input:checkbox').change(function () {
       if ($(this).is(':checked')) {
-        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+        amenitiesChecked[$(this).data('id')] = $(this).data('name');
       } else {
-        delete amenities[$(this).attr('data-id')];
+        delete amenitiesChecked[$(this).data('id')];
       }
-      $('.amenities H4').text(Object.values(amenities).join(', '));
+      const amenitiesList = Object.values(amenitiesChecked).join(', ');
+      $('.amenities h4').text(amenitiesList);
     });
   });
+  
